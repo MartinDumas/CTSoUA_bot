28ac359ef6fb6079cc6b4f5fecfc6bbbb7d16f8a
Oleg Malanchuk <olegmalanchuk2008@gmail.com>
2024-04-14 14:50:47 +0300

    oleg
    Co-Authored-By: Martin <126817324+MartinDumas@users.noreply.github.com>

--- CTSoUA_bot/src/main/java/com/example/CTSoUA_bot/CtSoUaBotApplication.java
+++ CTSoUA_bot/src/main/java/com/example/CTSoUA_bot/CtSoUaBotApplication.java
@@ -1,62 +1,19 @@
 package com.example.CTSoUA_bot;
 
+import com.example.CTSoUA_bot.controller.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class CtSoUaBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws TelegramApiException {
 		SpringApplication.run(CtSoUaBotApplication.class, args);
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-		System.out.printf("oleg");
-	}
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+		telegramBotsApi.registerBot(new TelegramBot());
 
+	}
 }
