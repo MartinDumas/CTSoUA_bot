68cc238ef7b3d0be11a45cf16dd1fd91d45a611c
Oleg Malanchuk <olegmalanchuk2008@gmail.com>
2024-04-15 18:11:35 +0300

    new first commit

--- CTSoUA_bot/src/main/java/com/example/CTSoUA_bot/controller/TelegramBot.java
+++ CTSoUA_bot/src/main/java/com/example/CTSoUA_bot/controller/TelegramBot.java
@@ -3,7 +3,10 @@ package com.example.CTSoUA_bot.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.name}")
@@ -16,19 +19,41 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
 //        return botName;
-        return "CTSoUA2_bot";
+        return "CTSoUA_bot";
     }
 
     @Override
     public String getBotToken() {
 //        return botToken;
-        return "6836705058:AAEeoeY8Abgpe9082pVz0lL4YUnIfysjdTk";
+        return "6959322213:AAGjboBmXBH3PixnTip8bT6ISYRfIiI_c7E";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
         System.out.println(originalMessage.getText());
+
+        String chatId = update.getMessage().getChatId().toString();
+        String text = update.getMessage().getText();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(text);
+
+        if (text.equals("/start")) {
+            sendMessage.setText("ви розпочали роботу з ботом");
+        } else if (text.equals("/help")) {
+            sendMessage.setText("help");
+        }
+
+
+        try {
+            this.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+
 
     }
 }
